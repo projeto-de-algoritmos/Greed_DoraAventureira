f1ddf68b9fb044012624eaa9362139527d169f84
Dâmaso Júnio <junio.pereirab@gmail.com>
2021-03-15 12:27:49 -0300

    Adding elements to screen (starting mobile first)
    Co-authored-by: Bruno Dantas <oliveiradantas96@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/dora.png';
+import comida from './assets/comida.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Dora Aventureira</h1>
+        <p>Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho Textinho </p>
+        <div className="image">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <input />
+            <button>Calcular</button>
+          </div>
+        </div>
+        <div className="options">
+          <button>
+            <img src={comida} alt= ""/>
+          </button>
+          <button>
+            <img src={comida} alt= ""/>
+          </button>
+          <button>
+            <img src={comida} alt= ""/>
+          </button>
+          <button>
+            <img src={comida} alt= ""/>
+          </button>
+          <button>
+            <img src={comida} alt= ""/>
+          </button>
+          <button>
+            <img src={comida} alt= ""/>
+          </button>
+          <button>
+            <img src={comida} alt= ""/>
+          </button>
+          <button>
+            <img src={comida} alt= ""/>
+          </button>
+          <button>
+            <img src={comida} alt= ""/>
+          </button>
+          <button>
+            <img src={comida} alt= ""/>
+          </button>
+        </div>
     </div>
   );
 }
